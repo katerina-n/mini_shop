d5c3ea522bc22f0bf035b30a2a9ede93f8b298a8
Katya <bpbpqdqd@gmail.com>
2018-08-09 11:15:38 +0300

    add api

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 namespace ApiBundle\Controller;
 
 
+use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,32 +16,30 @@ use Nelmio\ApiDocBundle\Annotation as API;
  * @Rest\Prefix("/info")
  * @Rest\NamePrefix("_info_")
  **/
-class DefaultController
+class DefaultController extends FOSRestController
 {
     /**
      * @Rest\Get("/{id}")
      *
      *
      * @SWG\Get(
-     *     summary="Get profile_info by Id.",
-     *     tags={"Profile_Info"},
-     *     @SWG\Parameter(name="id", in="path", description="profile_id", type="integer"),
+     *     summary="Get order_info by Id.",
+     *     tags={"Product_Info"},
+     *     @SWG\Parameter(name="id", in="path", description="product_id", type="integer"),
      *     @SWG\Response(
      *         response=200,
-     *         description="Return profile info",
-     *         @Model(type=\Diva\ProfileBundle\Entity\Profile::class, groups={"ApiProfile"})
-     *         ),
+     *         description="Return product info"),
      *     @SWG\Response(response="404", description="not found"),
      * )
      *
-     * @Rest\View(statusCode=200, serializerGroups={"ApiProfile"})
+     * @Rest\View(statusCode=200)
      *
      * @return
      *
      * @throws \Exception
      */
     public function getAction()
-    {
-        return 1;
+    { dump(1); exit;
+        return $this->getViewHandler();
     }
 }
